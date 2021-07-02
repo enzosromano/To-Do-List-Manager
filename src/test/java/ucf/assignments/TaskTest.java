617eb9d3f1b392fa0835609f11d839d8ee430626
enzosromano <enzo.romano@omicronmedia.com>
2021-07-01 21:53:29 -0400

    Updated test comments and pseudocode again

--- src/test/java/ucf/assignments/TaskTest.java
+++ src/test/java/ucf/assignments/TaskTest.java
@@ -5,44 +5,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskTest {
 
+    //Define a task here to be used below, probably empty
+
     @Test
-    void testGetName(){
+    void testGetAndSetName(){
+
+        //Set a name for a task using setName
+        //Use getName in order to test if the title name was set correctly
+        //and if getName pulls the name correctly
 
     }
     @Test
-    void testSetName(){
+    void testGetAndSetList(){
+
+        //Create a new task and using setList, assign it to a list
+        //Using that same task, use getList and check if it matches the name you expect
 
     }
     @Test
-    void testGetList(){
+    void testGetAndSetDescription(){
+
+        //using our previously created task with no defined attributes
+        //use set description to set the tasks description
+
+        //Using that same task, use getDescription to get the description and compare it to
+        //a hardcoded string you expect
 
     }
     @Test
-    void testSetList(){
+    void testGetAndSetDate(){
+
+        //Using our previously created task with no defined attributes
+        //use setDate to set the tasks due date
+
+        //Using that same task, use getDate to get the due date and compare it to
+        //a hardcoded date you expect
 
     }
     @Test
-    void testGetDescription(){
+    void testIsAndSetDone(){
 
-    }
-    @Test
-    void testSetDescription(){
-
-    }
-    @Test
-    void testGetDate(){
-
-    }
-    @Test
-    void testSetDate(){
-
-    }
-    @Test
-    void testIsDone(){
-
-    }
-    @Test
-    void testSetDone(){
+        //Using our task defined above, isDone will default to false so check if its false using isDone
+        //Is that passes, use setDone to set isDone to true
+        //using isDone check if the done value is now true as you expected
 
     }
 
