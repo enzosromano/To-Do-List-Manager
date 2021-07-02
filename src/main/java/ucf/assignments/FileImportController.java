42aba20d71310737765d9750cc147bdfd97dd7a1
enzosromano <enzo.romano@omicronmedia.com>
2021-07-02 17:33:01 -0400

    Added headers on files

---
+++ src/main/java/ucf/assignments/FileImportController.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Enzo Romano
+ */
+
+package ucf.assignments;
+
+public class FileImportController {
+
+
+
+}
--- src/main/java/ucf/assignments/MenuController.java
+++ src/main/java/ucf/assignments/MenuController.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Enzo Romano
+ */
+
 package ucf.assignments;
 
 import javafx.event.ActionEvent;
@@ -7,9 +12,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class MenuController {
@@ -35,6 +42,12 @@ public class MenuController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void importList(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog(stage);
     }
 
 }
