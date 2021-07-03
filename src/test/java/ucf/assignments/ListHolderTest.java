ba6d595f64483cd399d8b850fab8e3ed00a5aa1e
enzosromano <enzo.romano@omicronmedia.com>
2021-07-03 15:23:38 -0400

    Added pseudocode to our listholder tests

--- src/test/java/ucf/assignments/ListHolderTest.java
+++ src/test/java/ucf/assignments/ListHolderTest.java
@@ -1,7 +1,41 @@
 package ucf.assignments;
 
+import org.junit.jupiter.api.Test;
+
 public class ListHolderTest {
 
+    //Initialize a list of lists here which will be used for the first two tests
+
+    @Test
+    void testCreateList(){
+
+        //Create multiple lists by assigning multiple hardcoded values to a
+        //minimum of 5 new lists that will be stored in our list of lists
+        //which we have defined above
+
+    }
+    @Test
+    void testListsToFile(){
+
+        //Automate creating multiple lists and storing them in a list holder
+        //Once your list are created, create a path and file dependencies
+        //Iterate through our List of Lists and output them to our file created above
+
+    }
+    @Test
+    void testListsFromFile(){
+
+        //If needed, create a file that has the information we are looking for to
+        //create lists from
+
+        //This program will probably utilize CSV in order to store information in our file
+        //Iterate through the lines of our text file
+            //Split our text file by newlines and "," in order to get the names of each new list
+            //Use the create list function from our listHolder class to create each list
+            //in our iteration and store it in a list of lists
+
+
+    }
 
 
 }
